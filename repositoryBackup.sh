c5d01bfeac2c8b7ee1fdd0f566a7cd4c399482e1
fang <xufang5555@hotmail.com>
2016-10-30 18:26:38 -0400

    backup part completed

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+#* 1 * * * ./repositoryBackup.sh
 
+currentTime=`date +%Y%m%d`
+locaFolder=$1
+githubURL=$2
+githubUser=$3
+githubPass=$4
+backupName=backup$currentTime.tgz
+echo $backupName
+echo $locationFolder
+tar czvf $backupName $locaFolder --exclude='*.tgz'
+ 
+#git push https://git-githubUser:
+echo Backup $backupName created successfully!
+
+#scp
+
+#echo Backup $backupName committed to the local git repository
+
+
+
+#echo Backup $backupName pushed to the remote git repository $1
