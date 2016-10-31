e00c8c391baa400c9445d4a40096ad6435cd0dba
romansokolovski <r.sokolovski@gmail.com>
2016-10-31 19:53:02 -0400

    Update firewallSetup.sh

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Clear previous rules
+# Delete any previous rules in the INPUT, FORWARD and OUTPUT tables
 iptables -F INPUT
 iptables -F OUTPUT
 iptables -F FORWARD
 
-#Enable ssh only from computers in the range 130.15.00 to 130.15.255.2                              
+#Enable ssh only from computers in the range 130.15.0.0 to 130.15.255.255 [netmask /16]                            
 sudo iptables -A INPUT -p TCP --sport 22 -s 130.15.0.0/16 -j ACCEPT 
 
 #Enable ssh from computers in any private network
@@ -12,7 +12,7 @@ iptables -A INPUT -p TCP --sport 22 -s 192.168.0.0/16 -j ACCEPT
 iptables -A INPUT -p TCP --sport 22 -s 172.0.0.0/8 -j ACCEPT
 iptables -A INPUT -p TCP --sport 22 -s 10.0.0.0/8 -j ACCEPT
 
-# Enable HTTP[S]
+# Enable http and https from any IP
 iptables -A INPUT -p TCP --dport 80 -j ACCEPT
 iptables -A INPUT -p TCP --dport 443 -j ACCEPT
 iptables -A OUTPUT -p TCP --dport 80 -j ACCEPT
@@ -21,10 +21,10 @@ iptables -A OUTPUT -p TCP --dport 443 -j ACCEPT
 #Drop any other incoming tcp traffic
 iptables -A INPUT -p TCP -j DROP
 
-# Explicity allow connections out from 130.15.0.0 to 130.15.255.255
+#Block connecting from your computer to any IP outside the range 130.15.0.0 to 130.15.255.255 [netmask /16]
 iptables -A OUTPUT -p TCP -s 130.15.0.0/16 -j ACCEPT
 
-# Allow 130.15.100.100 to connect to mysql on 3306
+# Only allow IP 130.15.100.100 to connect to your mysql running on port 3306
 iptables -A INPUT -p TCP -s 130.15.100.100 --dport 3306 -j ACCEPT
 iptables -A OUTPUT -p TCP -s 130.15.100.100 --dport 3306 -j ACCEPT
 
