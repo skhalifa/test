1f18a103dff0e4de71eb02f5f0abab91ec1343ff
brandon <14blw@queensu.ca>
2016-10-31 22:07:46 -0400

    Question4

--- queensComputingSubdomainFinder.sh
+++ queensComputingSubdomainFinder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-domain="cs.queensu.ca"
-while read -r domain
-do
-	dig +short "$domain"
+for i in {0..255};do
+	for j in {0..255};do
+		addr=$(nslookup 130.15.$n.$m | grep 'name = ' | awk '{print $4}')
 done < /tmp/mylist | sort -u | awk '{printf "%s ", $0} END {printf "/n"}' > outputfile
 echo $outputfile
 
